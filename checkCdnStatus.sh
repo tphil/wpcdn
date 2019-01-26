e246c34c56ada645636cbaf08b638dee23ff1354
Theodore <tphilippidis@outlook.com>
2019-01-26 21:53:26 +0200

    added wp flush cache + ls purge

--- checkCdnStatus.sh
+++ checkCdnStatus.sh
@@ -3,7 +3,7 @@ while read -ru 4 LINE; do
     status=`curl $LINE -k -s -f -o /dev/null && echo "SUCCESS" || echo "ERROR"`
     if [ $status = "SUCCESS" ]
     then
-    	echo "Applying CDN congigs..."
+    	echo "Applying CDN configs..."
         continue
     else
     	echo "CDN is not ready yet."
@@ -12,6 +12,7 @@ while read -ru 4 LINE; do
 done 4< "$1"
 /usr/local/bin/wp lscache-admin set_option cdn true --allow-root --path="/var/www/webroot/ROOT" 
 crontab -l | sed "/checkCdnStatus/d" | crontab -
-
+wp cache flush  --path="/var/www/webroot/ROOT" 
+wp lscache-purge all --path="/var/www/webroot/ROOT" 
 rm -rf /home/litespeed/urls.txt  /home/litespeed/checkCdnStatus.sh
 echo "CDN is ready! Enjoy."
